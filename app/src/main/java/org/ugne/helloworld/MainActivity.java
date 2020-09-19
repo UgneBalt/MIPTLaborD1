56d8f1b1ff14580ffde4cd8adfdc72d6c236134e
UgneBalt <ugne33ugne@gmail.com>
2020-09-19 13:26:32 +0300

    II etapas - veikia du mygtukai

--- app/src/main/java/org/ugne/helloworld/MainActivity.java
+++ app/src/main/java/org/ugne/helloworld/MainActivity.java
@@ -1,7 +1,11 @@
 package org.ugne.helloworld;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +13,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button mygtukas1;
+    private Button mygtukas2;
     private TextView tekstas1;
 
     @Override
@@ -17,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mygtukas1 = findViewById(R.id.mygtukas1);
+        mygtukas2 = findViewById(R.id.mygtukas2);
         tekstas1 = findViewById(R.id.tekstas1);
 
         mygtukas1.setOnClickListener(new View.OnClickListener(){
@@ -24,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 tekstas1.setText("Hello world");
+            }
+        });
+
+        mygtukas2.setOnClickListener(new View.OnClickListener(){
+
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                tekstas1.setTextColor(Color.parseColor("#76eec6"));
             }
         });
     }
